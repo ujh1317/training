779bf384ab85f29fbeef29e537b77f9f5de38196
Yujeonghyeon <dbwjdgus1317@gmail.com>
2020-07-31 20:54:16 +0900

    7/31

---
+++ app/src/main/java/com/jeonghyeon/training/ViewPagerImageFragment.java
@@ -0,0 +1,34 @@
+package com.jeonghyeon.training;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class ViewPagerImageFragment extends Fragment{
+
+    public static ViewPagerImageFragment newInstance() {
+        
+        Bundle args = new Bundle();
+        
+        ViewPagerImageFragment fragment = new ViewPagerImageFragment();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.viewpager_imagefragment,container,false);
+        ImageView imageView = view.findViewById(R.id.imagefragment_imageview);
+        imageView.setImageResource(getArguments().getInt("imageUrl"));
+
+        return view;
+    }
+}
--- app/src/main/java/com/jeonghyeon/training/ViewPagerItemFragment.java
+++
@@ -1,6 +0,0 @@
-package com.jeonghyeon.training;
-
-import androidx.fragment.app.Fragment;
-
-public class ViewPagerItemFragment extends Fragment {
-}
