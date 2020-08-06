f81ab0e2e87f138fd2ad8487730b61871dbb6494
Yujeonghyeon <dbwjdgus1317@gmail.com>
2020-08-06 23:35:37 +0900

    8/06 add adapter

--- app/src/main/java/com/jeonghyeon/training/ViewPagerAdapter.java
+++ app/src/main/java/com/jeonghyeon/training/ViewPagerAdapter.java
@@ -1,4 +1,23 @@
 package com.jeonghyeon.training;
 
-public class ViewPagerAdapter {
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentStatePagerAdapter;
+
+public class ViewPagerAdapter extends FragmentStatePagerAdapter {
+    public ViewPagerAdapter(@NonNull FragmentManager fm) {
+        super(fm);
+    }
+
+    @NonNull
+    @Override
+    public Fragment getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public int getCount() {
+        return 0;
+    }
 }
