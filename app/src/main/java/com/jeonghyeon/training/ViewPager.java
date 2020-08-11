e1873df5b1b8dee0b768ce594edce17c0e85e768
Yujeonghyeon <dbwjdgus1317@gmail.com>
2020-08-11 23:43:20 +0900

    8/11 add adapter

---
+++ app/src/main/java/com/jeonghyeon/training/ViewPager.java
@@ -0,0 +1,15 @@
+package com.jeonghyeon.training;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ViewPager extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
