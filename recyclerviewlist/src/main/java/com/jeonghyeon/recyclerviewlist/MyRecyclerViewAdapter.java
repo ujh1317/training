5563d2a6f24a390266ffee1bd10cc798813def51
Yujeonghyeon <dbwjdgus1317@gmail.com>
2020-08-23 22:48:12 +0900

    8/23 adapter

--- recyclerviewlist/src/main/java/com/jeonghyeon/recyclerviewlist/MyRecyclerViewAdapter.java
+++ recyclerviewlist/src/main/java/com/jeonghyeon/recyclerviewlist/MyRecyclerViewAdapter.java
@@ -1,6 +1,9 @@
 package com.jeonghyeon.recyclerviewlist;
 
+import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -20,16 +23,28 @@ public class MyRecyclerViewAdapter extends RecyclerView.Adapter<RecyclerView.Vie
     @NonNull
     @Override
     public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        // XML 가져오는 부분
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.recyclerview_item,parent,false);
+        return new Rowcell(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+        // 데이터를 넣어주는 부분
 
     }
 
     @Override
     public int getItemCount() {
+        // 카운터
+
         return 0;
+    }
+
+    private class Rowcell extends RecyclerView.ViewHolder {
+
+        public Rowcell(View view) {
+            super(view);
+        }
     }
 }
