977425914b8476790a295b64db1c9778aafb0ec2
Yujeonghyeon <dbwjdgus1317@gmail.com>
2020-08-26 22:41:59 +0900

    8/26

--- recyclerviewlist/src/main/java/com/jeonghyeon/recyclerviewlist/MainActivity.java
+++ recyclerviewlist/src/main/java/com/jeonghyeon/recyclerviewlist/MainActivity.java
@@ -1,6 +1,8 @@
 package com.jeonghyeon.recyclerviewlist;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
@@ -10,5 +12,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        RecyclerView view = findViewById(R.id.main_recyclerview);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
+        view.setLayoutManager(layoutManager);
+        MyRecyclerViewAdapter myRecyclerViewAdapter = new MyRecyclerViewAdapter();
+        view.setAdapter(myRecyclerViewAdapter);
     }
 }
