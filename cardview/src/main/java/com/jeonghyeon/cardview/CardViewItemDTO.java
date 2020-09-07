ce2c45cf8d3024e7cddc7372a16fb7a001dfc8b3
Yujeonghyeon <dbwjdgus1317@gmail.com>
2020-09-07 22:59:31 +0900

    9/7 CardViewItemDTO

--- cardview/src/main/java/com/jeonghyeon/cardview/CardViewItemDTO.java
+++ cardview/src/main/java/com/jeonghyeon/cardview/CardViewItemDTO.java
@@ -1,4 +1,14 @@
 package com.jeonghyeon.cardview;
 
 public class CardViewItemDTO {
+
+    public int imageview;
+    public String title;
+    public String subtitle;
+
+    public CardViewItemDTO(int imageview, String title, String subtitle) {
+        this.imageview = imageview;
+        this.title = title;
+        this.subtitle = subtitle;
+    }
 }
