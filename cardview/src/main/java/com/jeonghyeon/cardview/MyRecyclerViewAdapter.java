a729fcc06c5db3fffa5d4031989b13b65b4418cd
Yujeonghyeon <dbwjdgus1317@gmail.com>
2020-09-08 22:31:51 +0900

    9/8 MyRecyclerViewAdapter

--- cardview/src/main/java/com/jeonghyeon/cardview/MyRecyclerViewAdapter.java
+++ cardview/src/main/java/com/jeonghyeon/cardview/MyRecyclerViewAdapter.java
@@ -5,7 +5,18 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.util.ArrayList;
+
 public class MyRecyclerViewAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+
+    private ArrayList<CardViewItemDTO> cardViewItemDTOs = new ArrayList<>();
+    public MyRecyclerViewAdapter() {
+
+        cardViewItemDTOs.add(new CardViewItemDTO(R.drawable.first,"첫번째","풍경1"));
+        cardViewItemDTOs.add(new CardViewItemDTO(R.drawable.second,"두번째","풍경2"));
+        cardViewItemDTOs.add(new CardViewItemDTO(R.drawable.third,"세번째","풍경3"));
+    }
+
     @NonNull
     @Override
     public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
