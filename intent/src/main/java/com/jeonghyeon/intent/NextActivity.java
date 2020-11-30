1b2b1efa5b02334e0e656c4aee46ec3a19291d72
Yujeonghyeon <dbwjdgus1317@gmail.com>
2020-11-30 23:24:33 +0900

    11/30 intent

--- intent/src/main/java/com/jeonghyeon/intent/NextActivity.java
+++ intent/src/main/java/com/jeonghyeon/intent/NextActivity.java
@@ -2,7 +2,10 @@ package com.jeonghyeon.intent;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class NextActivity extends AppCompatActivity {
 
@@ -10,5 +13,16 @@ public class NextActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_next);
+
+        Button button = findViewById(R.id.btnlogin);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(v.getContext(), NextActivity.class);
+
+                startActivity(intent);
+            }
+        });
+
     }
 }
