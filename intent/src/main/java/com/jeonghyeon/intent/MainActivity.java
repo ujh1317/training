ef0e02193b92b029fe1e0d6557838bcba9f80f8c
Yujeonghyeon <dbwjdgus1317@gmail.com>
2020-12-06 16:40:19 +0900

    12/06 intent

--- intent/src/main/java/com/jeonghyeon/intent/MainActivity.java
+++ intent/src/main/java/com/jeonghyeon/intent/MainActivity.java
@@ -2,7 +2,11 @@ package com.jeonghyeon.intent;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+            Button button = findViewById(R.id.btnlogin);
+            EditText id = findViewById(R.id.editText_id);
+            final EditText password = findViewById(R.id.editText_password);
+
+            button.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+
+                    if(password.getText().toString().equals("1234")) {
+                        Intent intent = new Intent(v.getContext(), NextActivity.class);
+                        intent.putExtra("id","이름");
+                        startActivity(intent);
+                    }else{
+                    }
+                }
+            });
+
+
+
+
     }
 }
--- intent/src/main/java/com/jeonghyeon/intent/NextActivity.java
+++ intent/src/main/java/com/jeonghyeon/intent/NextActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class NextActivity extends AppCompatActivity {
@@ -16,22 +17,7 @@ public class NextActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_next);
 
-        Button button = findViewById(R.id.btnlogin);
-        EditText id = findViewById(R.id.editText_id);
-        final EditText password = findViewById(R.id.editText_password);
-
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                if(password.getText().toString().equals("1234")) {
-                    Intent intent = new Intent(v.getContext(), NextActivity.class);
-                    intent.putExtra("id","이름");
-                    startActivity(intent);
-                }else{
-                }
-            }
-        });
+        TextView textView = findViewById(R.id.user_name);
 
     }
 }
