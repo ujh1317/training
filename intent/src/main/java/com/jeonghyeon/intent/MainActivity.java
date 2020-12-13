5bb9ae748518412363e1ce65cc8d44376d42bc95
Yujeonghyeon <dbwjdgus1317@gmail.com>
2020-12-13 22:49:15 +0900

    12/13 onActivityResult

--- intent/src/main/java/com/jeonghyeon/intent/MainActivity.java
+++ intent/src/main/java/com/jeonghyeon/intent/MainActivity.java
@@ -28,7 +28,7 @@ public class MainActivity extends AppCompatActivity {
                     if(password.getText().toString().equals("1234")) {
                         Intent intent = new Intent(v.getContext(), NextActivity.class);
                         intent.putExtra("id",id.getText().toString());
-                        startActivity(intent);
+                        startActivityForResult(intent,10);
                     }else{
                     }
                 }
@@ -36,7 +36,12 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        //만약 requestcode가 10이고 requestcode가 20인 경우 이 코드는 로그아웃한 사람이 누군지 알려주는 텍스트뷰를 작성
         super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 10 && requestCode == 20) {
+
+        }
+
     }
 }
--- intent/src/main/java/com/jeonghyeon/intent/NextActivity.java
+++ intent/src/main/java/com/jeonghyeon/intent/NextActivity.java
@@ -17,13 +17,16 @@ public class NextActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_next);
 
-        TextView textView = findViewById(R.id.user_name);
+        final TextView textView = findViewById(R.id.user_name);
         textView.setText(getIntent().getStringExtra("id"));
 
         Button button = findViewById(R.id.btn_close);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.putExtra("logout_user", textView.getText().toString()+"님이 로그아웃 하셨습니다.");
+                setResult(20,intent);
                 finish();
             }
         });
